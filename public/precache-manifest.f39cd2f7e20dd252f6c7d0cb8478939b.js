5c93c7d3dbc1eebe90ff2ae8688c0cc843101ab8
Denys Rebenok <denis.rebenok.pro@gmail.com>
2019-03-04 22:07:42 +0200

    deploy to heroku

---
+++ public/precache-manifest.f39cd2f7e20dd252f6c7d0cb8478939b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "40e102f1c9c2b82d926e",
+    "url": "/static/css/main.73ec7d36.chunk.css"
+  },
+  {
+    "revision": "40e102f1c9c2b82d926e",
+    "url": "/static/js/main.40e102f1.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d379602273310112c695",
+    "url": "/static/js/2.d3796022.chunk.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "fbe66ea2b32284ebdf20d56e0ef8e3c3",
+    "url": "/index.html"
+  }
+];
