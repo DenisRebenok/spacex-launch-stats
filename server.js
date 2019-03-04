6646c605ed394975ee08a1cc700cbd829b1752be
Denys Rebenok <denis.rebenok.pro@gmail.com>
2019-03-04 22:22:38 +0200

    add Launch component and finish the App

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.use(
     graphiql: true
   })
 );
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
